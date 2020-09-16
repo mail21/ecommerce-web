f3525b040c9883c639ac17528e1bf97d0082fb4d
mail21 <ismailidn1@gmail.com>
2020-09-16 09:51:48 +0700

    Menambah atribut state shoes

--- src/SHOES_DATA.js
+++ src/SHOES_DATA.js
@@ -1,43 +1,63 @@
 let SHOES_DATA = [
   {
+    id: 1,
+    like: false,
     nama: 'Mesh Slip On Air Cushion',
     src:
       'https://cdn.shopify.com/s/files/1/0076/8812/6522/products/slate8_1024x1024.jpg?v=1587464943',
   },
   {
+    id: 2,
+    like: false,
     nama: "PW 100 Men's Fitness",
     src:
       'https://contents.mediadecathlon.com/p1419921/72b461620e3e5ec61db942355362dd66/p1419921.jpg',
   },
   {
+    id: 3,
+    like: false,
     nama: 'Blue Sports Shoes',
     src: `https://assets.ajio.com/medias/sys_master/root/h9f/he0/13018714767390/-473Wx593H-460342492-blue-MODEL.jpg`,
   },
   {
+    id: 4,
+    like: false,
     nama: 'Shoes Bank White Sneaker',
     src: `https://rukminim1.flixcart.com/image/714/857/jmwch3k0/shoe/j/y/n/dg-292-white-blue-patti-10-digitrendzz-white-original-imaf9p36fkykfjqt.jpeg?q=50`,
   },
   {
+    id: 5,
+    like: false,
     nama: 'ASIAN Wonder-13 Grey Firozi',
     src: `https://images-na.ssl-images-amazon.com/images/I/61utX8kBDlL._UL1100_.jpg`,
   },
   {
+    id: 6,
+    like: false,
     nama: `Nike Men's Revolution 5`,
     src: `https://fgl.scene7.com/is/image/FGLSportsLtd/FGL_332979914_01_a?bgColor=0,0,0,0&fmt=png-alpha&hei=528&resMode=sharp&qlt=85,1&op_sharpen=1`,
   },
   {
+    id: 7,
+    like: false,
     nama: 'BILT Dexter Shoes',
     src: `https://www.cyclegear.com/_a/product_images/0187/1698/bilt_dexter_textile_motorcycle_shoes_750x750.jpg`,
   },
   {
+    id: 8,
+    like: false,
     nama: `Denill Women's Sneaker`,
     src: `https://images-na.ssl-images-amazon.com/images/I/71x0W0bURGL._UY500_.jpg`,
   },
   {
+    id: 9,
+    like: false,
     nama: 'GOrun MaxRoad 4 Hyper',
     src: `https://vader-prod.s3.amazonaws.com/1570119748-screen-shot-2019-10-03-at-12-21-52-pm-1570119723.png`,
   },
   {
+    id: 10,
+    like: false,
     nama: 'Skechers Arch Fit',
     src: `https://www.skechers.com/on/demandware.static/-/Sites-skechers-master/default/dw4a6da04b/images/large/232040_WNV.jpg`,
   },
